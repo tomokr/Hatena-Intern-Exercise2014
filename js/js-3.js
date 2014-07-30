0cb9c8f5a7db6f3c73f6ed92e3f67d3315d73558
tomokr <tomok@Tomo-no-MacBook-Air.local>
2014-07-30 10:02:08 +0900

    JS3課題

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,9 @@
 // 課題 JS-3 の実装をここに記述してください。
+element = document.getElementById("submit-button");
+element.addEventListener("click",createTable,false);
+
+function createTable(){
+	text = document.getElementById("log-input").value;
+	parent_elem = document.getElementById("table-container")
+	 createLogTable(parent_elem,parseLTSVLog(text));
+}
